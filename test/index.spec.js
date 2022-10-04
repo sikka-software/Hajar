f390d5c0d793e8b5c187241aed169210b570946a
Zied El Ifa <ziedelifa@gmail.com>
2022-10-04 17:58:10 +0100

    fix build error

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,7 +4,7 @@ import Hajar from '../src';
 describe('Hajar.src.js', () => {
 
   it('should get the library\'s version', () => {
-    expect(Hajar.version).toEqual('1.0.12');
+    expect(Hajar.version).toEqual('1.0.13');
   });
 
   it('should get the library\'s name', () => {
