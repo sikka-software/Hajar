889e69bd7e2f603b8ba4cb0d9c479da38b53acda
Ben Mansour Mansour <44977033+Mansourkira@users.noreply.github.com>
2023-06-02 18:06:59 +0100

    SIKKA-4304[IN PROGRESS] SIKKA-4305[IN PROGRESS]

--- src/core/models/index.js
+++ src/core/models/index.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 const { exec } = require("child_process");
+const readline = require("readline");
+const path = require("path");
 
 function generateModelsFromJSON(jsonFilePath) {
   // Read the JSON file
@@ -16,8 +18,6 @@ function generateModelsFromJSON(jsonFilePath) {
   for (const modelName in models) {
     const modelProperties = models[modelName];
     let modelContent = "";
-
-    // Generate model content
     modelContent += `const mongoose = require("mongoose");\n`;
     modelContent += `const { Schema } = mongoose;\n\n`;
     modelContent += `const ${modelName}Schema = new Schema({\n`;
@@ -31,21 +31,86 @@ function generateModelsFromJSON(jsonFilePath) {
     modelContent += `});\n\n`;
     modelContent += `const ${modelName} = mongoose.model("${modelName.toLowerCase()}", ${modelName}Schema);\n\n`;
     modelContent += `module.exports = ${modelName};\n`;
-
     // Write model content to a file
-    fs.writeFileSync(`./models/${modelName}.js`, modelContent);
+    const modelsPath = path.join(process.cwd(), "models");
+    createDirectory(modelsPath);
 
-    // Run any additional command if required (e.g., migrations)
-    exec(`npm run migrate:model ${modelName}`, (error, stdout, stderr) => {
-      if (error) {
-        console.error(
-          `Error running migration for ${modelName}: ${error.message}`
-        );
-        return;
-      }
-      console.log(`Migration for ${modelName} completed successfully!`);
-    });
+    const modelFilePath = path.join(modelsPath, `${modelName}.js`);
+    writeFile(modelFilePath, modelContent);
+    // Generate schema and resolvers
+    const schemaContent = generateSchemaContent(modelName, modelProperties);
+    const resolverContent = generateResolverContent(modelName);
+
+    // Write schema and resolver content to files
+    const projectPath = path.join(process.cwd(), "graphQl");
+    const resolversPath = path.join(projectPath, "resolvers");
+    const typesPath = path.join(projectPath, "types");
+
+    createDirectory(projectPath);
+    createDirectory(resolversPath);
+    createDirectory(typesPath);
+
+    const schemaFilePath = path.join(typesPath, `${modelName}.type.graphql`);
+    const resolverFilePath = path.join(
+      resolversPath,
+      `${modelName}.resolver.js`
+    );
+
+    writeFile(schemaFilePath, schemaContent);
+    writeFile(resolverFilePath, resolverContent);
+
+    console.log(
+      `Schema and resolvers for ${modelName} generated successfully!`
+    );
   }
+}
+
+function generateSchemaContent(modelName, modelProperties) {
+  let schemaContent = `type ${modelName} {\n`;
+  for (const propertyName in modelProperties) {
+    const propertyType = modelProperties[propertyName];
+    schemaContent += `  ${propertyName}: ${propertyType}\n`;
+  }
+  schemaContent += `}\n\n`;
+
+  schemaContent += `type Query {\n`;
+  schemaContent += `  get${modelName}(id: ID!): ${modelName}\n`;
+  schemaContent += `  getAll${modelName}s: [${modelName}]\n`;
+  schemaContent += `}\n\n`;
+
+  return schemaContent;
+}
+
+function generateResolverContent(modelName) {
+  let resolverContent = `const ${modelName} = require("../models/${modelName}.js");\n\n`;
+  resolverContent += `const resolvers = {\n`;
+  resolverContent += `  Query: {\n`;
+  resolverContent += `    get${modelName}: async (parent, { id }) => {\n`;
+  resolverContent += `      return ${modelName}.findById(id);\n`;
+  resolverContent += `    },\n`;
+  resolverContent += `    getAll${modelName}s: async () => {\n`;
+  resolverContent += `      return ${modelName}.find();\n`;
+  resolverContent += `    },\n`;
+  resolverContent += `  },\n`;
+  resolverContent += `};\n\n`;
+  resolverContent += `module.exports = resolvers;\n`;
+
+  return resolverContent;
+}
+
+function createDirectory(directoryPath) {
+  if (!fs.existsSync(directoryPath)) {
+    fs.mkdirSync(directoryPath, { recursive: true });
+  }
+}
+
+function writeFile(filePath, content) {
+  fs.writeFileSync(filePath, content, (error) => {
+    if (error) {
+      console.error(`Error writing file: ${filePath}`, error);
+      return;
+    }
+  });
 }
 
 module.exports = generateModelsFromJSON;
