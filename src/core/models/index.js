09926b902bf9daab7cedf5a11ee3dee2d3a35d13
Ben Mansour Mansour <44977033+Mansourkira@users.noreply.github.com>
2023-06-07 15:31:01 +0100

    Hajar Generate Models SIKKA-4304[COMPLETED]

--- src/core/models/index.js
+++ src/core/models/index.js
@@ -7,6 +7,141 @@ function generateModelsFromJSON(jsonFilePath) {
   // Read the JSON file
   let models = null;
   try {
+    const fs = require("fs");
+    const { exec } = require("child_process");
+    const path = require("path");
+
+    function generateModelsFromJSON(jsonFilePath) {
+      // Read the JSON file
+      let models = null;
+      try {
+        const jsonData = fs.readFileSync(jsonFilePath, "utf8");
+        models = JSON.parse(jsonData);
+      } catch (error) {
+        console.error("Error reading or parsing the models JSON file:", error);
+        process.exit(1);
+      }
+
+      // Generate models
+      for (const modelName in models) {
+        const modelProperties = models[modelName];
+        let modelContent = "";
+        modelContent += `const mongoose = require("mongoose");\n`;
+        modelContent += `const { Schema } = mongoose;\n\n`;
+        modelContent += `const ${modelName}Schema = new Schema({\n`;
+        for (const propertyName in modelProperties) {
+          const propertyType = modelProperties[propertyName];
+          modelContent += `  ${propertyName}: {\n`;
+          modelContent += `    type: ${propertyType},\n`;
+          modelContent += `    required: true,\n`;
+          modelContent += `  },\n`;
+        }
+        modelContent += `});\n\n`;
+        modelContent += `const ${modelName} = mongoose.model("${modelName.toLowerCase()}", ${modelName}Schema);\n\n`;
+        modelContent += `module.exports = ${modelName};\n`;
+        // Write model content to a file
+        const modelsPath = path.join(process.cwd(), "models");
+        createDirectory(modelsPath);
+
+        const modelFilePath = path.join(modelsPath, `${modelName}.js`);
+        writeFile(modelFilePath, modelContent);
+        // Generate schema and resolvers
+        const schemaContent = generateSchemaContent(modelName, modelProperties);
+        const resolverContent = generateResolverContent(modelName);
+
+        // Write schema and resolver content to files
+        const projectPath = path.join(process.cwd(), "graphQl");
+        const resolversPath = path.join(projectPath, "resolvers");
+        const typesPath = path.join(projectPath, "types");
+
+        createDirectory(projectPath);
+        createDirectory(resolversPath);
+        createDirectory(typesPath);
+
+        const schemaFilePath = path.join(
+          typesPath,
+          `${modelName}.type.graphql`
+        );
+        const resolverFilePath = path.join(
+          resolversPath,
+          `${modelName}.resolver.js`
+        );
+
+        writeFile(schemaFilePath, schemaContent);
+        writeFile(resolverFilePath, resolverContent);
+
+        console.log(
+          `Schema and resolvers for ${modelName} generated successfully!`
+        );
+      }
+    }
+    function generateSchemaContent(modelName, modelProperties) {
+      let schemaContent = `type ${modelName} {\n`;
+      for (const propertyName in modelProperties) {
+        const propertyType = modelProperties[propertyName];
+        schemaContent += `  ${propertyName}: ${propertyType}\n`;
+      }
+      schemaContent += `}\n\n`;
+
+      schemaContent += `type Query {\n`;
+      schemaContent += `  get${modelName}(id: ID!): ${modelName}\n`;
+      schemaContent += `  getAll${modelName}s: [${modelName}]\n`;
+      schemaContent += `}\n\n`;
+
+      return schemaContent;
+    }
+
+    function generateResolverContent(modelName) {
+      let resolverContent = `const ${modelName} = require("../../models/${modelName}.js");
+  
+  const resolvers = {
+    Query: {
+      get${modelName}: async (parent, { id }) => {
+        try {
+          const result = await ${modelName}.findById(id);
+          if (!result) {
+            throw new Error("${modelName} not found.");
+          }
+          return result;
+        } catch (error) {
+          console.error(error);
+          throw new Error("Failed to get ${modelName}. Please try again later.");
+        }
+      },
+      getAll${modelName}s: async () => {
+        try {
+          return await ${modelName}.find();
+        } catch (error) {
+          console.error(error);
+          throw new Error("Failed to get ${modelName}s. Please try again later.");
+        }
+      },
+    },
+  };
+  
+  module.exports = resolvers;
+  `;
+
+      return resolverContent;
+    }
+
+    function createDirectory(directoryPath) {
+      if (!fs.existsSync(directoryPath)) {
+        fs.mkdirSync(directoryPath, { recursive: true });
+      }
+    }
+
+    function writeFile(filePath, content) {
+      fs.writeFileSync(filePath, content, (error) => {
+        if (error) {
+          console.error(`Error writing file: ${filePath}`, error);
+          return;
+        }
+      });
+    }
+
+    module.exports = generateModelsFromJSON;
+
     const jsonData = fs.readFileSync(jsonFilePath, "utf8");
     models = JSON.parse(jsonData);
   } catch (error) {
