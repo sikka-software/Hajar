702b49ac424a5a447b8ab0113595d9d1b9bb4aac
Ben Mansour Mansour <44977033+Mansourkira@users.noreply.github.com>
2023-06-17 02:28:24 +0100

    Update Generate Schema + Resolvers

--- src/core/models/index.js
+++ src/core/models/index.js
@@ -77,7 +77,7 @@ function generateModelsFromJSON(jsonFilePath) {
     );
   }
 }
-
+// this is to generate the Schema
 function generateSchemaContent(modelName, modelProperties) {
   let schemaContent = `type ${modelName} {\n`;
   for (const propertyName in modelProperties) {
@@ -86,21 +86,143 @@ function generateSchemaContent(modelName, modelProperties) {
       schemaContent += `  ${propertyName}: ID\n`; // Use ID type for ObjectId
     } else if (propertyType === "[ObjectId]") {
       schemaContent += `  ${propertyName}: [ID]\n`; // Use ID type for ObjectId
+    } else if (propertyType === "String") {
+      schemaContent += `  ${propertyName}: String\n`; // Use String type
+    } else if (propertyType === "Int" || propertyType === "Number") {
+      schemaContent += `  ${propertyName}: Int\n`; // Use Int type
+    } else if (propertyType === "Float") {
+      schemaContent += `  ${propertyName}: Float\n`; // Use Float type
+    } else if (propertyType === "Boolean") {
+      schemaContent += `  ${propertyName}: Boolean\n`; // Use Boolean type
+    } else if (propertyType === "[String]") {
+      schemaContent += `  ${propertyName}: [String]\n`; // Use array of String type
+    } else if (propertyType === "[Float]") {
+      schemaContent += `  ${propertyName}: [Float]\n`; // Use array of Float type
+    } else if (propertyType === "[Boolean]") {
+      schemaContent += `  ${propertyName}: [Boolean]\n`; // Use array of Boolean type
+    } else if (typeof propertyType === "object") {
+      // Handle nested fields
+      schemaContent += `  ${propertyName}: ${generateNestedType(
+        propertyType
+      )}\n`;
     } else {
-      schemaContent += `  ${propertyName}: ${propertyType}\n`;
+      schemaContent += `  ${propertyName}: ${propertyType}\n`; // Use the provided type
+    }
+  }
+  schemaContent += `}\n\n`;
+
+  schemaContent += `input ${modelName}Input {\n`;
+  for (const propertyName in modelProperties) {
+    const propertyType = modelProperties[propertyName];
+    if (propertyType === "ObjectId") {
+      schemaContent += `  ${propertyName}: ID\n`; // Use ID type for ObjectId
+    } else if (propertyType === "[ObjectId]") {
+      schemaContent += `  ${propertyName}: [ID]\n`; // Use ID type for ObjectId
+    } else if (propertyType === "String") {
+      schemaContent += `  ${propertyName}: String\n`; // Use String type
+    } else if (propertyType === "Int" || propertyType === "Number") {
+      schemaContent += `  ${propertyName}: Int\n`; // Use Int type
+    } else if (propertyType === "Float") {
+      schemaContent += `  ${propertyName}: Float\n`; // Use Float type
+    } else if (propertyType === "Boolean") {
+      schemaContent += `  ${propertyName}: Boolean\n`; // Use Boolean type
+    } else if (propertyType === "[String]") {
+      schemaContent += `  ${propertyName}: [String]\n`; // Use array of String type
+    } else if (propertyType === "[Float]") {
+      schemaContent += `  ${propertyName}: [Float]\n`; // Use array of Float type
+    } else if (propertyType === "[Boolean]") {
+      schemaContent += `  ${propertyName}: [Boolean]\n`; // Use array of Boolean type
+    } else if (typeof propertyType === "object") {
+      // Handle nested fields
+      schemaContent += `  ${propertyName}: ${generateNestedInput(
+        propertyType
+      )}\n`;
+    } else {
+      schemaContent += `  ${propertyName}: ${propertyType}\n`; // Use the provided type
     }
   }
   schemaContent += `}\n\n`;
 
   schemaContent += `type Query {\n`;
-  schemaContent += `   get${modelName}ById(id: ID! , roleID : ID!): ${modelName}\n`;
+  schemaContent += `  get${modelName}ById(id: ID!, roleID: ID!): ${modelName}\n`;
   schemaContent += `  getAll${modelName}s(roleID: ID!): [${modelName}]\n`;
   schemaContent += `  count${modelName}s(): Int\n`;
+  schemaContent += `}\n\n`;
+
+  schemaContent += `type Mutation {\n`;
+  schemaContent += `  create${modelName}(input: ${modelName}Input): ${modelName}\n`;
+  schemaContent += `  update${modelName}(id: ID!, input: ${modelName}Input): ${modelName}\n`;
+  schemaContent += `  delete${modelName}(id: ID!): ${modelName}\n`;
   schemaContent += `}\n\n`;
 
   return schemaContent;
 }
 
+function generateNestedType(nestedFields) {
+  let nestedTypeContent = "{\n";
+  for (const propertyName in nestedFields) {
+    const propertyType = nestedFields[propertyName];
+    if (propertyType === "ObjectId") {
+      nestedTypeContent += `    ${propertyName}: ID\n`; // Use ID type for ObjectId
+    } else if (propertyType === "[ObjectId]") {
+      nestedTypeContent += `    ${propertyName}: [ID]\n`; // Use ID type for ObjectId
+    } else if (propertyType === "String") {
+      nestedTypeContent += `    ${propertyName}: String\n`; // Use String type
+    } else if (propertyType === "Int" || propertyType === "Number") {
+      nestedTypeContent += `    ${propertyName}: Int\n`; // Use Int type
+    } else if (propertyType === "Float") {
+      nestedTypeContent += `    ${propertyName}: Float\n`; // Use Float type
+    } else if (propertyType === "Boolean") {
+      nestedTypeContent += `    ${propertyName}: Boolean\n`; // Use Boolean type
+    } else if (propertyType === "[String]") {
+      nestedTypeContent += `    ${propertyName}: [String]\n`; // Use array of String type
+    } else if (propertyType === "[Int]") {
+      nestedTypeContent += `    ${propertyName}: [Int]\n`; // Use array of Int type
+    } else if (propertyType === "[Float]") {
+      nestedTypeContent += `    ${propertyName}: [Float]\n`; // Use array of Float type
+    } else if (propertyType === "[Boolean]") {
+      nestedTypeContent += `    ${propertyName}: [Boolean]\n`; // Use array of Boolean type
+    } else {
+      nestedTypeContent += `    ${propertyName}: ${propertyType}\n`; // Use the provided type
+    }
+  }
+  nestedTypeContent += "  }";
+  return nestedTypeContent;
+}
+
+function generateNestedInput(nestedFields) {
+  let nestedInputContent = "{\n";
+  for (const propertyName in nestedFields) {
+    const propertyType = nestedFields[propertyName];
+    if (propertyType === "ObjectId") {
+      nestedInputContent += `    ${propertyName}: ID\n`; // Use ID type for ObjectId
+    } else if (propertyType === "[ObjectId]") {
+      nestedInputContent += `    ${propertyName}: [ID]\n`; // Use ID type for ObjectId
+    } else if (propertyType === "String") {
+      nestedInputContent += `    ${propertyName}: String\n`; // Use String type
+    } else if (propertyType === "Int") {
+      nestedInputContent += `    ${propertyName}: Int\n`; // Use Int type
+    } else if (propertyType === "Float") {
+      nestedInputContent += `    ${propertyName}: Float\n`; // Use Float type
+    } else if (propertyType === "Boolean") {
+      nestedInputContent += `    ${propertyName}: Boolean\n`; // Use Boolean type
+    } else if (propertyType === "[String]") {
+      nestedInputContent += `    ${propertyName}: [String]\n`; // Use array of String type
+    } else if (propertyType === "[Int]") {
+      nestedInputContent += `    ${propertyName}: [Int]\n`; // Use array of Int type
+    } else if (propertyType === "[Float]") {
+      nestedInputContent += `    ${propertyName}: [Float]\n`; // Use array of Float type
+    } else if (propertyType === "[Boolean]") {
+      nestedInputContent += `    ${propertyName}: [Boolean]\n`; // Use array of Boolean type
+    } else {
+      nestedInputContent += `    ${propertyName}: ${propertyType}\n`; // Use the provided type
+    }
+  }
+  nestedInputContent += "  }";
+  return nestedInputContent;
+}
+
+// Here we generate the resolver content
 function generateResolverContent(modelName) {
   let resolverContent = `const ${modelName} = require("../../models/${modelName}.js");
 const RoleModel = require("../../models/Role.js");
@@ -195,7 +317,62 @@ const resolvers = {
       return count;
     },
   },
+
+  Mutation: {
+    create${modelName}: async (parent,{input}) => {
+      try {
+        console.log("Resolving create${modelName} mutation...");
+
+        const ${modelName.toLowerCase()} = await ${modelName}.create({input});
+        return { ...${modelName.toLowerCase()}._doc };
+
+        }catc (errorcatchCreate${modelName}) {
+        console.log("Something went wrong during creating ${modelName}.", errorcatchCreate${modelName});
+        return new GraphQLError("Something went wrong during creating ${modelName}.", {
+          extensions: { code: "server-error" },
+        });
+      }
+    },
+    update${modelName}: async (parent, {id , input}) => {
+      try {
+        console.log("Resolving update${modelName} mutation...");
+        const ${modelName.toLowerCase()} = await ${modelName}.findByIdAndUpdate(
+          id,
+          input,
+          { new: true }
+        )
+
+        if (!${modelName.toLowerCase()}) {
+          throw new Error("${modelName} not found.");
+        }
+        return { ...${modelName.toLowerCase()}._doc};
+      } catch (errorcatchUpdate${modelName}) {
+        console.log("Something went wrong during updating ${modelName}.", errorcatchUpdate${modelName});
+        return new GraphQLError("Something went wrong during updating ${modelName}.", {
+          extensions: { code: "server-error" },
+        });
+      }
+    },
+    delete${modelName}: async (parent, {id}) => {
+      try {
+        console.log("Resolving delete${modelName} mutation...");
+        const ${modelName.toLowerCase()} = await ${modelName}.findByIdAndDelete(id);
+
+        if (!${modelName.toLowerCase()}) {
+          throw new Error("${modelName} not found.");
+        }
+        return ${modelName.toLowerCase()}
+      } catch (errorcatchDelete${modelName}) {
+        console.log("Something went wrong during deleting ${modelName}.", errorcatchDelete${modelName});
+        return new GraphQLError("Something went wrong during deleting ${modelName}.", {
+          extensions: { code: "server-error" },
+        });
+      }
+    },
+  },
 };
+
+
 
 module.exports = resolvers;
 `;
