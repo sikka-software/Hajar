4681dbe7067c4b301eecbc6fcac9efdbdc95bca3
Ben Mansour Mansour <44977033+Mansourkira@users.noreply.github.com>
2023-07-07 17:25:49 +0100

    update package.json

---
+++ User.resolver.js
@@ -0,0 +1,25 @@
+
+  const User = require('./models/User');
+  module.exports = {
+    Query: {
+        async getUsers() {
+            return await User.find();
+        },
+        async getUser(_, { id }) {
+            return await User.findById({_id : id});
+        }
+    },
+    Mutation: {
+        async createUser(_, { input }) {
+            const newUser = new User(input);
+            return await newUser.save();
+        },
+        async updateUser(_, { id, input }) {
+            return await User.findByIdAndUpdate(id, input, { new: true });
+        },
+        async deleteUser(_, { id }) {
+            return await User.findByIdAndDelete({_id :id});
+        }
+    }
+};
+  
