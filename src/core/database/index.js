2d0d5743d5a97ace196a80c04bc252ffdbf505b5
zaaakher <zakhermasri@gmail.com>
2023-10-05 23:49:07 +0300

    Added more docs pages

--- src/core/database/index.js
+++ src/core/database/index.js
@@ -1,3 +1,11 @@
+/**
+ * Set up and configure a database connection based on the specified type.
+ *
+ * @param {string} type - The type of database (e.g., "MongoDB", "SQL", "MySQL").
+ * @param {object} options - Configuration options specific to the chosen database.
+ * @param {object} middleware - Middleware or library for connecting to the database (e.g., Mongoose for MongoDB).
+ * @returns {object|null} - A database connection object or `null` if the type is not recognized.
+ */
 function setupDatabase(type, options, middleware) {
   switch (type) {
     case "MongoDB":
@@ -9,12 +17,18 @@ function setupDatabase(type, options, middleware) {
     case "Supabase":
       return setupSupabase(options);
     case "Firebase Database":
-      return setupFirebaseDatabase(options);
+      return setupFirebaseDB(options);
     default:
       return null;
   }
 }
-
+/**
+ * Set up and configure a MongoDB connection.
+ *
+ * @param {object} options - Configuration options for MongoDB (e.g., URL, options).
+ * @param {object} middleware - The MongoDB middleware or library (e.g., Mongoose).
+ * @returns {object} - A MongoDB connection object.
+ */
 function setupMongoDB(options, middleware) {
   const connection = middleware.connect(options.url, {
     useNewUrlParser: true,
@@ -22,23 +36,44 @@ function setupMongoDB(options, middleware) {
   });
   return connection;
 }
-
+/**
+ * Set up and configure an SQL database connection.
+ *
+ * @param {object} options - Configuration options for the SQL database.
+ * @returns {object} - An SQL database connection object.
+ */
 function setupSQL(options) {
   // Connect to SQL using the options provided
   // ...
 }
 
+/**
+ * Set up and configure a MySQL database connection.
+ *
+ * @param {object} options - Configuration options for the MySQL database.
+ * @returns {object} - A MySQL database connection object.
+ */
 function setupMySQL(options) {
   // Connect to MySQL using the options provided
   // ...
 }
-
+/**
+ * Set up and configure a Supabase database connection.
+ *
+ * @param {object} options - Configuration options for Supabase.
+ * @returns {object} - A Supabase database connection object.
+ */
 function setupSupabase(options) {
   // Connect to Supabase using the options provided
   // ...
 }
-
-function setupFirebaseDatabase(options) {
+/**
+ * Set up and configure a Firebase Database connection.
+ *
+ * @param {object} options - Configuration options for Firebase Database.
+ * @returns {object} - A Firebase Database connection object.
+ */
+function setupFirebaseDB(options) {
   // Connect to Firebase Database using the options provided
   // ...
 }
