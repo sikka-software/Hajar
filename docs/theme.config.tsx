0939e7c0c71ba8d9746330ab2ba4ba457d2ba0b4
zaaakher <zakhermasri@gmail.com>
2023-10-06 10:01:49 +0300

    updated favicon [skip_publish, deploy_docs]

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -24,6 +24,7 @@ const config: DocsThemeConfig = {
 
   head: (
     <>
+      <link rel="icon" type="image/png" href="./favicon.ico" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta
         name="description"
