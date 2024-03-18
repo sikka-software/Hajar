8fcb878e11b7880c061c0813674ae8793a55e893
mansourkira <mansour.benmansour@esprit.tn>
2024-03-18 13:29:38 +0100

    chore : update spec test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,8 @@
-import hajar from "../www/src/@sikka/hajar/index";
+const hajar = require("../www/src/@sikka/hajar/index");
+
+const pkg = require("../package.json");
 describe("Hajar.src.js", () => {
   it("should get the library's version", () => {
-    expect(hajar.version).toEqual("1.0.41");
+    expect(hajar.version).toEqual(pkg.version);
   });
 });
