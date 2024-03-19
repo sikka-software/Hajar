602269b0a32af7611e1740bc51b6ad13fb708a3a
mansourkira <mansour.benmansour@esprit.tn>
2024-03-19 20:09:32 +0100

    chore : update version

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,6 +3,6 @@
 import { version } from "../package.json";
 describe("Hajar.src.js", () => {
   it("should get the library's version", () => {
-    expect(["1.1.66-beta", "1.1.66"]).toContain(version);
+    expect(["1.1.67-beta", "1.1.67"]).toContain(version);
   });
 });
