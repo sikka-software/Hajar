0069640d7c2c33d3dd593802ba146ea0f5ff3c5f
mansourkira <mansour.benmansour@esprit.tn>
2024-03-19 11:34:13 +0100

    chore : publish package
    SIKKA-6593[in progress]

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,6 +3,6 @@
 import { version } from "../package.json";
 describe("Hajar.src.js", () => {
   it("should get the library's version", () => {
-    expect(version).toEqual("1.1.63-beta");
+    expect(["1.1.63-beta", "1.1.63"]).toContain(version);
   });
 });
