4b0bdfb1bbc45f868eee4a6428cca3dc291a71cd
mansourkira <mansour.benmansour@esprit.tn>
2024-03-20 13:43:12 +0100

    fix buid : update config files
    SIKKA-6593[in progress]

--- .babelrc.js
+++ .babelrc.js
@@ -1,11 +1,13 @@
 const { NODE_ENV } = process.env;
 
-export const presets = [
-  [
-    "@babel/preset-env",
-    {
-      modules: NODE_ENV === "test" ? "auto" : false,
-    },
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        modules: NODE_ENV === "test" ? "auto" : false,
+      },
+    ],
   ],
-];
-export const plugins = ["@babel/plugin-proposal-object-rest-spread"];
+  plugins: ["@babel/plugin-proposal-object-rest-spread"],
+};
