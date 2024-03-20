08775c87ea7b9ecc021a3a2ee091ef87605540c8
mansourkira <mansour.benmansour@esprit.tn>
2024-03-20 01:36:08 +0100

    chore : update config files

--- rollup.config.js
+++ rollup.config.js
@@ -21,7 +21,7 @@ const makeConfig = (env = "development") => {
   let bundleSuffix = env === "production" ? "min." : "";
 
   return {
-    input: "./www/src/@sikka/hajar/index.js",
+    input: "./www/src/@sikka/hajar/core/index.ts",
     external: EXTERNAL,
     output: [
       {
